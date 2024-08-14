a96446d4cfd932491243bcf5b81200694b3308b1
Jessica F. Martinez <jessicafndez@gmail.com>
2024-08-14 10:22:34 +0200

    ADD build folder

---
+++ dist/oeb-widgets-graphs.cjs.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const e=require("./widget-element-CE5FCFBi.cjs");exports.WidgetElement=e.WidgetElement;
---
+++ dist/oeb-widgets-graphs.es.js
@@ -0,0 +1,4 @@
+import { W as m } from "./widget-element-gK5o17Et.js";
+export {
+  m as WidgetElement
+};
